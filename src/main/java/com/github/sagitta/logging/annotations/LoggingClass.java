fd10c03832974dddb8e892cc3d3bd8e2129eed77
Noman5237 <anonyman637@gmail.com>
2023-12-07 02:25:15 +0600

    Added patches

--- src/main/java/com/github/sagitta/logging/annotations/LoggingClass.java
+++ src/main/java/com/github/sagitta/logging/annotations/LoggingClass.java
@@ -1,14 +1,12 @@
-package com.sagitta.logging.annotations;
-
+package com.github.sagitta.logging.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target (ElementType.METHOD)
+@Target (ElementType.TYPE)
 @Retention (RetentionPolicy.RUNTIME)
-public @interface NoLogging {
+public @interface LoggingClass {
 
 }
-
--- src/main/java/com/github/sagitta/logging/annotations/NoLogging.java
+++ src/main/java/com/github/sagitta/logging/annotations/NoLogging.java
@@ -1,12 +1,14 @@
-package com.sagitta.logging.annotations;
+package com.github.sagitta.logging.annotations;
+
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target (ElementType.TYPE)
+@Target (ElementType.METHOD)
 @Retention (RetentionPolicy.RUNTIME)
-public @interface LoggingClass {
+public @interface NoLogging {
 
 }
+
